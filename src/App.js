52214b4b34968aa9fc6013fbd6b61c4f5243e943
Giuseppe <giuseppeuhlmann@gmail.com>
2022-06-14 17:08:34 -0300

    feat: adicionando cor de fundo e título

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Giuseppe Portfolio</p>
+    <div className="h-screen bg-neutral-800 flex items-center justify-center">
+      <div className="text-center text-3xl">
+        <p className="text-white font-[raleway] font-semibold">Olá, meu nome é <span className="text-blue-600 font-bold">Giuseppe Nunes.</span></p>
+        <p className="text-white font-[raleway] font-semibold">Estudante de Desenvolvimento Web Full-Stack</p>
+      </div>
     </div>
   );
 }
