0e88f10a86ac2f65628bd1f6fc1f25724e819fdc
Giuseppe <giuseppeuhlmann@gmail.com>
2022-06-15 12:45:35 -0300

    feat: adicionando css de menu selecionado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
+  const location = useLocation();
+  const [active, setActive] = useState({
+    about: false,
+    port: false,
+    contact: false,
+  })
+
+  useEffect(() => {
+    const path = location.pathname.split('/')[1];
+
+    switch(path) {
+      case 'portfolio':
+        setActive({port: true, contact: false, about: false });
+        break;
+      case 'contato':
+        setActive({port: false, contact: true, about: false });
+        break;
+      default:
+        setActive({port: false, contact: false, about: true });
+        break;
+    }
+  }, [location])
+
   return(
     <nav className="flex justify-center bg-black p-5">
       <ul className="flex justify-around w-96 text-xl text-white">
         <Link to="/">
-          <li className="uppercase font-semibold hover:text-cyan-500 transition-all duration-300">Sobre Mim</li>
+          <li className={ active.about ? 'active' : ''}>Sobre Mim</li>
         </Link>
         <Link to="/portfolio">
-          <li className="uppercase font-semibold hover:text-cyan-500 transition-all duration-300">Portfolio</li>
+          <li className={ active.port ? 'active' : ''}>Portfolio</li>
         </Link>
         <Link to="/contato">
-          <li className="uppercase font-semibold hover:text-cyan-500 transition-all duration-300">Contato</li>
+          <li className={ active.contact ? 'active' : ''}>Contato</li>
         </Link>
       </ul>
     </nav>
