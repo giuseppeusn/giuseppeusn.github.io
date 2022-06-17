ebeec5d472afc3d5489b4ba9d5fec848d8fdf1fc
Giuseppe <giuseppeuhlmann@gmail.com>
2022-06-16 23:12:45 -0300

    feat: alterando estilo da página de contato

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -35,7 +35,7 @@ function Contact() {
   return (
     <div className="bg-neutral-800">
       <Header />
-      <div className="h-screen">
+      <div className="h-[80vh]">
         <div className="flex justify-center mt-20">
           <h1 className="text-3xl border-b-2 p-3 text-white font-semibold uppercase">
             <span className="text-cyan-500 text-3xl font-semibold">{'<'}</span>
@@ -44,37 +44,39 @@ function Contact() {
             <span className="text-cyan-500 text-3xl font-semibold">{'>'}</span>
           </h1>
         </div>
-        <div className="flex flex-col items-center mt-20 text-3xl bg-neutral-800">
-          <h1 className="text-lg text-cyan-500 w-80 text-center">Tem uma dúvida/sugestão ou uma oportunidade de trabalho/projeto?</h1>
+        <div className="flex flex-col items-center mt-20 text-3xl">
+          <h1 className="text-lg text-cyan-500 w-[420px] text-center">Tem uma dúvida/sugestão ou uma oportunidade de trabalho/projeto?</h1>
           <form
             onSubmit={sendEmail}
-            className="bg-[#1e1e1e] flex flex-col justify-around items-center mt-10 h-96 w-[500px] p-8 rounded-lg text-white"
-          > 
-            <input
-              type="text"
-              placeholder='Nome'
-              className="text-lg font-light p-2 border-b-2 border-cyan-800 bg-[#212121] w-[90%]
-              outline-none focus:border-cyan-500 transition-all duration-300 placeholder-white"
-              name="name"
-            />
-            <input 
-              type="email"
-              placeholder='Email'
-              className="text-lg font-light p-2 border-b-2 border-cyan-800 bg-[#212121] w-[90%]
-              outline-none focus:border-cyan-500 transition-all duration-300 placeholder-white"
-              name="email"
-            />
+            className="flex flex-col justify-around items-center mt-10 h-80 w-[600px] p-8 rounded-lg"
+          >
+            <div className="flex items-around w-[90%]">
+              <input
+                type="text"
+                placeholder='Nome'
+                className="text-lg font-light p-2 border-b-2 border-cyan-800 bg-[#212121] w-[50%] mr-3
+                outline-none focus:border-cyan-500 transition-all duration-300 placeholder-white text-white"
+                name="name"
+              />
+              <input 
+                type="email"
+                placeholder='Email'
+                className="text-lg font-light p-2 border-b-2 border-cyan-800 bg-[#212121] w-[50%]
+                outline-none focus:border-cyan-500 transition-all duration-300 placeholder-white text-white"
+                name="email"
+              />
+            </div>
             <textarea
               placeholder='Sua mensagem'
               className="text-lg font-light h-24 border-b-2 border-cyan-800 p-2 bg-[#212121] w-[90%]
-              outline-none focus:border-cyan-500 transition-all duration-300 placeholder-white"
+              outline-none focus:border-cyan-500 transition-all duration-300 placeholder-white text-white"
               name="message"
             />
             { !load ? (
                 <button
                   type="submit"
                   className="flex items-center justify-center text-lg w-48 border border-cyan-500 p-3
-                  hover:bg-cyan-500 transition-all duration-300"
+                  text-white hover:bg-cyan-500 transition-all duration-300"
                 >
                   Enviar
                 </button>
