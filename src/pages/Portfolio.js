b6b88157b16023f4b57c88d78bdb5591b48b5dee
Giuseppe <giuseppeuhlmann@gmail.com>
2022-06-19 00:43:55 -0300

    fix: remove card projeto teste e corrige erro na página de portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -24,33 +24,33 @@ function Portfolio() {
       <p ref={ref}></p>
       <div ref={content} className="h-[120vh] flex flex-col bg-neutral-800 pb-20 mt-[1px]">
         {
-          contentVisible && <Title title="portfolio" />
+          contentVisible ? <Title title="portfolio" /> : <span className="mt-32 h-[62px]"></span>
         }
-          <div className="flex justify-center mt-20">
-            <div className="grid grid-cols-3 gap-4">
-            {
-              projects.map((e, i) => (
-                <div className="wrap" style={{ "--order": i }}>
-                  <img src={ e.thumb.url } alt={e.thumb.alt} className="h-[400px] object-cover" />
-                  <div className="image_overlay">
-                    <div className="flex flex-col items-center">
-                      <p className="font-bold">{e.name}</p>
-                      <p className="text-cyan-500 text-base">{e.tags}</p>
-                    </div>
-                    <div>
-                      <button
-                        className="flex items-center justify-around text-lg w-56 border border-cyan-500 p-3
-                        hover:bg-cyan-500 transition-all duration-300"
-                        id={e.id}
-                        onClick={ openModal }
-                      >
-                      Ver mais
-                      </button>
-                    </div>
+        <div className="flex justify-center mt-20">
+          <div className="grid grid-cols-3 gap-4">
+          {
+            projects.map((e, i) => (
+              <div className="wrap" style={{ "--order": i }}>
+                <img src={ e.thumb.url } alt={e.thumb.alt} className="h-[400px] object-cover" />
+                <div className="image_overlay">
+                  <div className="flex flex-col items-center">
+                    <p className="font-bold">{e.name}</p>
+                    <p className="text-cyan-500 text-base">{e.tags}</p>
+                  </div>
+                  <div>
+                    <button
+                      className="flex items-center justify-around text-lg w-56 border border-cyan-500 p-3
+                      hover:bg-cyan-500 transition-all duration-300"
+                      id={e.id}
+                      onClick={ openModal }
+                    >
+                    Ver mais
+                    </button>
                   </div>
                 </div>
-              ))
-            }
+              </div>
+            ))
+          }
           </div>
         </div>
       </div>
