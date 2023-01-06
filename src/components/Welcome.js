884e3aa04b643a37c7cecbde62f60e7286b032bb
giuseppeusn <giuseppeuhlmann@gmail.com>
2023-01-06 19:52:52 -0300

    refactor: change welcome button text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -105,10 +105,10 @@ function Welcome() {
             <div className="title-anim flex mt-8 opacity-0" style={ { "--order": 1} }>
               <button
                 onClick={executeScroll}
-                className="flex items-center justify-around text-lg w-56 border border-cyan-500 p-3
+                className="flex items-center justify-around text-lg w-48 border border-cyan-500 p-3
                 hover:bg-cyan-500 transition-all duration-300"
               >
-                View my projects
+                Learn more
                 <AiOutlineArrowDown className="text-2xl" />
               </button>
             </div>
