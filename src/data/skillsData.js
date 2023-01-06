af06f37b407a91f86b468cef90f334c734fdd23d
giuseppeusn <giuseppeuhlmann@gmail.com>
2023-01-06 16:27:32 -0300

    refactor: add docs links and some css in about page

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -4,23 +4,28 @@ const skillsData = [
     skills: [
       {
         name: 'JavaScript',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
       },
       {
         name: 'TypeScript',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+        url: 'https://www.typescriptlang.org/docs/'
       },
       {
         name: 'Python',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+        url: 'https://docs.python.org/3/'
       },
       {
         name: 'HTML5',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/HTML'
       },
       {
         name: 'CSS3',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
       }
     ]
   },
@@ -29,23 +34,28 @@ const skillsData = [
     skills: [
       {
         name: 'React',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+        url: 'https://reactjs.org/docs/getting-started.html'
       },
       {
         name: 'Redux',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+        url: 'https://redux.js.org/introduction/getting-started'
       },
       {
         name: 'Tailwind CSS',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+        url: 'https://tailwindcss.com/docs'
       },
       {
         name: 'Jest',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+        url: 'https://jestjs.io/docs/getting-started'
       },
       {
         name: 'RTL',
-        image: 'https://testing-library.com/img/octopus-64x64.png'
+        image: 'https://testing-library.com/img/octopus-64x64.png',
+        url: 'https://testing-library.com/docs/react-testing-library/intro'
       }
     ]
   },
@@ -54,19 +64,23 @@ const skillsData = [
     skills: [
       {
         name: 'Node.js',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+        url: 'https://nodejs.org/api/'
       },
       {
         name: 'Express.js',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+        url: 'https://expressjs.com/en/api.html'
       },
       {
         name: 'MySQL',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
+        url: 'https://dev.mysql.com/doc/'
       },
       {
         name: 'Mocha & Chai',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg',
+        url: 'https://mochajs.org/'
       }
     ]
   },
@@ -75,7 +89,14 @@ const skillsData = [
     skills: [
       {
         name: 'Arduino',
-        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg'
+        image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg',
+        url: 'https://www.arduino.cc/reference/en/'
+      },
+      {
+        name: 'ESP8266',
+        image: 'https://www.makielectronic.com/fotos/grandes/esp8266.png',
+        url: 'https://www.espressif.com/en/products/socs/esp8266',
+        class: 'h-4 w-full mt-[9px]'
       }
     ]
   }
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -15,7 +15,7 @@ function AboutMe() {
     <div>
       <Header />
       <p ref={ref}></p>
-      <div ref={content} className="h-screen bg-neutral-800 mt-[1px]">
+      <div ref={content} className="bg-neutral-800 mt-[1px]">
       {  
         contentVisible &&
         <div className="flex flex-col justify-center">
@@ -28,9 +28,9 @@ function AboutMe() {
                 <span className="text-cyan-500"> me</span>
                 ?
               </span>
-              <p className="text-white w-96 text-center mt-5">
-                I'm a Front-end developer, graduated in IT technician,
-                student of Mechatronics Engineering and student of Full-stack Web Development at Trybe.
+              <p className="text-white w-[400px] text-center mt-5">
+                I'm a Full-stack Web Developer, graduated in IT technician and
+                student of Mechatronics Engineering.
                 <br/>
                 <br/>
                 Driven by curiosity, I love technology and innovation. I'm interested in innovative and high-impact projects,
@@ -39,22 +39,30 @@ function AboutMe() {
             </div>
             <div className="text-white">
               <h1 className="text-3xl uppercase font-bold text-center">My <span className="text-cyan-500">skills</span></h1>
-              <ul className="mt-5">
+              <ul className="my-5 flex flex-col items-center">
                 {
                   skillsData.map((type) => (
-                    <li className="text-xl font-semibold mt-10 text-center border-b-2 border-black border-opacity-20">
+                    <li
+                      className="text-xl font-semibold mt-10 text-center border-b-2 border-black
+                      border-opacity-20 cursor-pointer w-fit"
+                    >
                       {type.typeSkill}
                       <div className="flex justify-around mt-5">
                         {
                           type.skills.map((skill) => (
-                            <span className="flex flex-col items-center p-2 text-sm hover:text-cyan-500">
-                                <img
-                                src={skill.image}
-                                alt={`${skill.name} icon`}
-                                className="w-8"
-                              />
-                              <span className="mt-2">{skill.name}</span>
-                            </span>
+                            <a href={skill.url} target="_blank" rel="noreferrer">
+                              <span
+                                className="flex flex-col items-center p-3 text-sm hover:text-cyan-500
+                                min-h-[84px] justify-between"
+                              >
+                                  <img
+                                  src={skill.image}
+                                  alt={`${skill.name} icon`}
+                                  className={skill.class ? skill.class : "w-8"}
+                                />
+                                <span className="mt-2">{skill.name}</span>
+                              </span>
+                            </a>
                           ))
                         }
                       </div>
